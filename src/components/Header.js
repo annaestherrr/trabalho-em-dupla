ff6f9ba25187cda4fc91d70ce61a913cd36b79d5
annaestherrr <annaesther_15@hotmail.com>
2022-06-21 19:54:36 -0300

    header e main1 estilizados

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,61 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Header(){
-  return(
+const Cabeçario = styled.div`
+  background-image: url("https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:e048e30a-811b-49f2-a92f-1af9a2985abd;revision=0?component_id=8444393a-5d93-4997-8ad0-0d450a8011e0&api_key=CometServer1&access_token=1655802322_urn%3Aaaid%3Asc%3AUS%3Ae048e30a-811b-49f2-a92f-1af9a2985abd%3Bpublic_002815fc6cc23418ab69f4ba32f51b1059f48863");
+  background-size: 100vw 100vh;
+  background-repeat: no-repeat;
+  height: 100vh;
+  margin: 1rem;
+`;
+
+const T2Header = styled.h2`
+  color: #373737;
+  font-size: 3rem;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+`;
+
+const ItemsHeader = styled.ul`
+  width: 30vw;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  color: #373737;
+  font-size: 1rem;
+`;
+
+const Title = styled.h1`
+  height: 85%;
+  font-weight: 500;
+  color: #373737;
+  font-size: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function Header() {
+  return (
     <>
-    <h1>Estou na header</h1>
-    <p>oi</p>
+      <Cabeçario>
+        <Menu>
+          <T2Header>RC</T2Header>
+          <ItemsHeader>
+            <li>ABOUT</li>
+            <li>RECIPES</li>
+            <li>SUBSCRIBE</li>
+          </ItemsHeader>
+        </Menu>
+        <Title>RECIPES</Title>
+      </Cabeçario>
     </>
-  )
+  );
 }
--- src/components/Main1.js
+++ src/components/Main1.js
@@ -1,10 +1,106 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Main(){
-  return(
+const S1 = styled.section`
+  color: #373737;
+  font-size: 3rem;
+`;
+
+const ContainerH2 = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding-top: 10rem;
+`;
+
+const Borda = styled.div`
+  width: 3rem;
+  border-bottom: #373737 solid;
+  margin-top: 0.8rem;
+`;
+
+const ContainerFood = styled.ul`
+  margin-top: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  img {
+    width: 22rem;
+    height: 15rem;
+  }
+`;
+
+const ContainerText = styled.div`
+  border: red solid;
+  width: 22rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 5rem;
+`;
+
+const ItemFood = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const TitleImage = styled.h3`
+  width: 18rem;
+  text-align: center;
+  font-size: 1.3rem;
+  font-weight: 500;
+  padding-top: 1rem;
+`;
+
+export default function Main() {
+  return (
     <>
-    <h1>Estou na main</h1>
-    <p>DOug</p>
+      <main1>
+        <S1>
+          <ContainerH2>
+            <h2>LATEST RECIPES</h2>
+            <Borda></Borda>
+          </ContainerH2>
+          <ContainerFood>
+            <ItemFood>
+              <img
+                src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:e048e30a-811b-49f2-a92f-1af9a2985abd;revision=0?component_id=3f630898-5430-4084-a530-83df642c4b76&api_key=CometServer1&access_token=1655802322_urn%3Aaaid%3Asc%3AUS%3Ae048e30a-811b-49f2-a92f-1af9a2985abd%3Bpublic_002815fc6cc23418ab69f4ba32f51b1059f48863"
+                alt=""
+              />
+              <ContainerText>
+                <Borda></Borda>
+                <TitleImage>Red Velvet Cake</TitleImage>
+              </ContainerText>
+            </ItemFood>
+            <ItemFood>
+              <img
+                src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:e048e30a-811b-49f2-a92f-1af9a2985abd;revision=0?component_id=6940ecee-9497-47c7-bc32-30dc58ecb408&api_key=CometServer1&access_token=1655802322_urn%3Aaaid%3Asc%3AUS%3Ae048e30a-811b-49f2-a92f-1af9a2985abd%3Bpublic_002815fc6cc23418ab69f4ba32f51b1059f48863"
+                alt=""
+              />
+              <ContainerText>
+                <Borda></Borda>
+                <TitleImage>Margherita Pizza</TitleImage>
+              </ContainerText>
+            </ItemFood>
+            <ItemFood>
+              <img
+                src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:e048e30a-811b-49f2-a92f-1af9a2985abd;revision=0?component_id=bf663e2d-434a-4d4d-8185-a9a0e5dce49d&api_key=CometServer1&access_token=1655802322_urn%3Aaaid%3Asc%3AUS%3Ae048e30a-811b-49f2-a92f-1af9a2985abd%3Bpublic_002815fc6cc23418ab69f4ba32f51b1059f48863"
+                alt=""
+              />
+              <ContainerText>
+                <Borda></Borda>
+                <TitleImage>Peanut Smoothie</TitleImage>
+              </ContainerText>
+            </ItemFood>
+          </ContainerFood>
+        </S1>
+        <section></section>
+      </main1>
     </>
-  )
+  );
 }
