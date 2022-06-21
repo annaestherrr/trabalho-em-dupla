898ad635b7094e28fa5556d9bfa9c30224b8e7bf
Douglas <douglinhasuniverso@gmail.com>
2022-06-21 19:14:58 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App(){
     <>
     <Header />
     <Main1 />
+    
     </>
   )
 }
--- src/components/Main1.js
+++ src/components/Main1.js
@@ -4,6 +4,7 @@ export default function Main(){
   return(
     <>
     <h1>Estou na main</h1>
+    <p>DOug</p>
     </>
   )
 }
