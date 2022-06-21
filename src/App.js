bdebdd604870e3930c6fe935a8b3290d64cab983
annaestherrr <annaesther_15@hotmail.com>
2022-06-21 19:06:27 -0300

    criei a header e a main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header"
+import Main1 from "./components/Main1"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <>
+    <Header />
+    <Main1 />
+    </>
+  )
 }
-
-export default App;
